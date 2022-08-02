046782dc1974057ecdf8c6cdf5a1dda4c995ffde
Yoooum <akyomi@qq.com>
2022-08-02 18:15:21 +0800

    Fix

--- install_cn/install_panel.sh
+++ install_cn/install_panel.sh
@@ -540,8 +540,8 @@ Install_Bt(){
 	chmod -R +x ${setup_path}/server/panel/script
 	ln -sf /etc/init.d/bt /usr/bin/bt
 	echo "${panelPort}" > ${setup_path}/server/panel/data/port.pl
-	wget -O /etc/init.d/bt https://ghproxy.com/https://raw.githubusercontent.com/Yoooum/btpanel-v7.7.0/main/install_cn/src/bt7.init -T 10
-	wget -O /www/server/panel/init.sh https://ghproxy.com/https://raw.githubusercontent.com/Yoooum/btpanel-v7.7.0/main/install_cn/src/bt7.init -T 10
+	wget -O /etc/init.d/bt https://ghproxy.com/https://raw.githubusercontent.com/Yoooum/btpanel-v7.7.0/main/install/src/bt7.init -T 10
+	wget -O /www/server/panel/init.sh https://ghproxy.com/https://raw.githubusercontent.com/Yoooum/btpanel-v7.7.0/main/install/src/bt7.init -T 10
 	wget -O /www/server/panel/data/softList.conf ${download_Url}/install/conf/softList.conf
 }
 Set_Bt_Panel(){
